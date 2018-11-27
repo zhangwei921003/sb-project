2ab0c3a98b8375e7884de8aeb044e3f264e1558d
zhangwei <15711366039@163.com>
2018-11-27 22:14:33 +0800

    ut

---
+++ springboot-unit-test/src/main/java/com/purchasing/springbootunittest/SpringbootUnitTestApplication.java
@@ -0,0 +1,12 @@
+package com.purchasing.springbootunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUnitTestApplication.class, args);
+	}
+}
